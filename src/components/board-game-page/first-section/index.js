36eda963367fa8f435603b3ae34fb0692cb416e1
Nick_Ermak <nk.ermak@yandex.ru>
2019-08-07 20:51:02 +0300

    feat(*): first section tags

--- src/components/board-game-page/first-section/index.js
+++ src/components/board-game-page/first-section/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDice } from '@fortawesome/free-solid-svg-icons'
 
 import './first-section.css';
 
@@ -8,7 +10,14 @@ class FirstSection extends React.Component {
             <div className='first-section'>
 
                 <div className='first-section__content-wrapper'>
-                    First Section
+                    <h1 className='first-section__game-heading'>Монополия</h1>
+                    <p className='first-section__game-description'>Покупайте и богатейте! Но это, конечно же, не так просто.</p>
+                    <div className='first-section__icon-wrapper'>
+                        <FontAwesomeIcon className='first-section__icon' icon={faDice} />
+                        <FontAwesomeIcon className='first-section__icon' icon={faDice} />
+                        <FontAwesomeIcon className='first-section__icon' icon={faDice} />
+                    </div>
+                    <button className='first-section__buy-button'>Купить</button>
                 </div>
             </div>
         )
