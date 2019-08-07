1ff39957b5ae4ba1ca8dbbdcd0f6d32a2c4f33b3
Антон Степанок <antonstepanok@MacBook-Pro-Anton.local>
2019-08-07 10:21:04 +0300

    feat(*): init

---
+++ src/components/board-game-page/first-section/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './first-section.css';
+
+class FirstSection extends React.Component {
+    render() {
+        return (
+            <div className='first-section'>
+
+                <div className='first-section__content-wrapper'>
+                    First Section
+                </div>
+            </div>
+        )
+    }
+}
+
+export default FirstSection;
