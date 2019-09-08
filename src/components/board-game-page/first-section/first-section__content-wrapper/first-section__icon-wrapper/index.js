61f95d047525876be4eb5fe6bf23a187acd9bba7
Nick_Ermak <nk.ermak@yandex.ru>
2019-09-08 18:30:23 +0300

    'different__displays'

--- src/components/board-game-page/first-section/first-section__content-wrapper/first-section__icon-wrapper/index.js
+++ src/components/board-game-page/first-section/first-section__content-wrapper/first-section__icon-wrapper/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faDice } from '@fortawesome/free-solid-svg-icons'
-import { faUserFriends } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 import { faHourglassHalf } from '@fortawesome/free-solid-svg-icons'
 
 import './first-section__icon-wrapper.css';
@@ -10,9 +10,18 @@ class FirstSection__IconWrapper extends React.Component {
     render() {
         return (
             <div className='first-section__icon-wrapper'>
-                <FontAwesomeIcon className='first-section__icon' icon={faDice} />
-                <FontAwesomeIcon className='first-section__icon' icon={faUserFriends} />
-                <FontAwesomeIcon className='first-section__icon' icon={faHourglassHalf} />
+                <div className='first-section__game-parameter'>
+                    <FontAwesomeIcon className='first-section__icon' icon={faDice} />
+                    <p className='first-section__argument'>от 2 до 6 игроков</p>
+                </div>
+                <div className='first-section__game-parameter'>
+                    <FontAwesomeIcon className='first-section__icon' icon={faUser} />
+                    <p className='first-section__argument'>от 8 лет</p>
+                </div>
+                <div className='first-section__game-parameter'>
+                    <FontAwesomeIcon className='first-section__icon' icon={faHourglassHalf} />
+                    <p className='first-section__argument'>от 60 до 90 минут</p>
+                </div>
             </div>
         )
     }
